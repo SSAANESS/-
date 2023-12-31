5d8c2976de0d62826c5f6bc1c58748b75830b1c7
SSAANESS <154890526+SSAANESS@users.noreply.github.com>
2023-12-31 12:19:14 +0300

    Create Quizhp.py
    Просто квиз  по Гарри Поттеру

---
+++ Quizhp.py
@@ -0,0 +1,185 @@
+print('Привет, ты сюда припёрся для игры по Гарри Поттеру? ')
+print('Ну тогда поехали!')
+questions = [
+             '1.Как звали крестного папу Гарри Поттера?',
+             '2. Настоящее имя Волан-де-Морта?',
+             '3. Как в книгах и фильме называли людей, которые лишены магических способностей?',
+             '4. Какое у Гарри второе имя?',
+             '5. Когда студенты Хогвартса сдают экзамен СОВ — Супер Отменное Волшебство ?',
+             '6. Кто из Уизли погибает во время финальной битвы за Хогвартс?',
+             '7. Какой журнал издает отец Полумны Лавгуд, Ксенофилиус?',
+             '8. Какое заклинание заставляет левитировать объекты в воздухе?',
+             '10. Когда у Гарри Поттера день рождения?',
+             '11. Каков правильный порядок рождения детей Уизли?',
+             '12. Кто анонимно отправляет Гарри мантию-невидимку на первое Рождество в Хогвартсе?',
+             '13. Какое животное является символом факультета Когтевран Школы Чародейства и Волшебства Хогвартс?',
+             '14. Как зовут собаку Хагрида?',
+             '15. Как называется семейный дом Уизли?',
+             '16. Какое зелье приносит удачу тому, кто его выпьет?',
+             '17. Какая девичья фамилия сестер Лили Поттер и Петунии Дурсль?',
+             '18. В какой день умерли Джеймс и Лили Поттер?',
+             '19. Что преподает профессор Макгонагалл?',
+             '20. На ком женится Билл Уизли?'
+]
+answers = [
+          'Сириус Блэк',
+          'Том Марволо Реддл',
+          'маглы',
+          'Джеймс',
+          'в конце 5-го курса ',
+          'Фред Уизли',
+          '«Придира»',
+          'Вингардиум Левиоса',
+          'Лондон, площадь Гриммо, 12',
+          '31 июля 1980 года',
+          'Билл, Чарли, Перси, Фред, Джордж, Рон, Джинни',
+          'Альбус Дамблдор',
+          'орел',
+          'Клык',
+          'Нора',
+          'Феликс Фелицис',
+          'Эванс',
+          '31 октября',
+          'трансфигурация',
+          'Флёр Делакур'
+]
+
+score = 0
+
+print(questions[0])
+user_input = input('Введи свой ответ: ')
+if user_input.lower() == answers[0].lower():
+    print('Правильно!')
+    score = score + 1
+else:
+    print('Неправильно')
+
+print(questions[1])
+user_input = input('Введи свой ответ: ')
+if user_input.lower() == answers[1].lower():
+    print('Правильно!')
+    score = score + 1
+else:
+    print('Неправильно')
+
+print(questions[2])
+user_input = input('Введи свой ответ: ')
+if user_input.lower() == answers[2].lower():
+    print('Правильно!')
+    score = score + 1
+else:
+    print('Неправильно')
+
+print(questions[3])
+user_input = input('Введи свой ответ: ')
+if user_input.lower() == answers[3].lower():
+    print('Правильно!')
+    score = score + 1
+else:
+    print('Неправильно')
+
+print(questions[4])
+user_input = input('Введи свой ответ: ')
+if user_input.lower() == answers[4].lower():
+    print('Правильно!')
+    score = score + 1
+else:
+    print('Неправильно')
+print(questions[5])
+user_input = input('Введи свой ответ: ')
+if user_input.lower() == answers[5].lower():
+    print('Правильно!')
+    score = score + 1
+else:
+    print('Неправильно')
+print(questions[6])
+user_input = input('Введи свой ответ: ')
+if user_input.lower() == answers[6].lower():
+    print('Правильно!')
+    score = score + 1
+else:
+    print('Неправильно')
+print(questions[7])
+user_input = input('Введи свой ответ: ')
+if user_input.lower() == answers[7].lower():
+    print('Правильно!')
+    score = score + 1
+else:
+    print('Неправильно')
+print(questions[8])
+user_input = input('Введи свой ответ: ')
+if user_input.lower() == answers[8].lower():
+    print('Правильно!')
+    score = score + 1
+else:
+    print('Неправильно')
+print(questions[9])
+user_input = input('Введи свой ответ: ')
+if user_input.lower() == answers[9].lower():
+    print('Правильно!')
+    score = score + 1
+else:
+    print('Неправильно')
+print(questions[10])
+user_input = input('Введи свой ответ: ')
+if user_input.lower() == answers[10].lower():
+    print('Правильно!')
+    score = score + 1
+else:
+    print('Неправильно')
+print(questions[11])
+user_input = input('Введи свой ответ: ')
+if user_input.lower() == answers[11].lower():
+    print('Правильно!')
+    score = score + 1
+else:
+    print('Неправильно')
+print(questions[12])
+user_input = input('Введи свой ответ: ')
+if user_input.lower() == answers[12].lower():
+    print('Правильно!')
+    score = score + 1
+else:
+    print('Неправильно')
+print(questions[13])
+user_input = input('Введи свой ответ: ')
+if user_input.lower() == answers[13].lower():
+    print('Правильно!')
+    score = score + 1
+else:
+    print('Неправильно')
+    print(questions[14])
+user_input = input('Введи свой ответ: ')
+if user_input.lower() == answers[14].lower():
+    print('Правильно!')
+    score = score + 1
+else:
+    print('Неправильно')
+print(questions[15])
+user_input = input('Введи свой ответ: ')
+if user_input.lower() == answers[15].lower():
+    print('Правильно!')
+    score = score + 1
+else:
+    print('Неправильно')
+print(questions[16])
+user_input = input('Введи свой ответ: ')
+if user_input.lower() == answers[16].lower():
+    print('Правильно!')
+    score = score + 1
+else:
+    print('Неправильно')
+print(questions[17])
+user_input = input('Введи свой ответ: ')
+if user_input.lower() == answers[18].lower():
+    print('Правильно!')
+    score = score + 1
+else:
+    print('Неправильно')
+
+if score > 15:
+    print("Это отличный результат! Крутотень! А ты шаришь! ")
+if score > 8:
+    print("Круто! Ты по идее знаешь многое, но многое тебе предстоит узнать!")
+else:
+    print(" Видимо, ты только-только втянулся в вселенную. Но ничего, всё впереди!  ")
